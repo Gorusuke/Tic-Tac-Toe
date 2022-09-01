dfb8a26cdbacbb7eb833452f7573e8f8edd4be5c
Jose Andres <Gorusuke123@gmail.com>
2022-08-31 21:26:12 -0500

    fixing some issues and having a issue with reset game

--- App.js
+++ App.js
@@ -1,230 +1,183 @@
 const $ = (selector) => document.querySelector(selector)
-const $$ = (selector) => document.querySelectorAll(selector)
+const $$ = (selector) => document.getElementById(selector)
 
-// const playerOne = document.getElementById('select-player-1')
-// const playerTwo = document.getElementById('select-player-2')
 const playerOne = $('#select-player-1')
 const playerTwo = $('#select-player-2')
 const gameSelection = $('.game-selection')
 const gameBoard = $('.game-board')
 const winner = $('.game-winner')
-const cells = $$('.game-cell')
-const playAgain = $$('.game-reset')
-
-let user;
-let computer;
-const winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [6, 4, 2], [2, 5, 8], [1, 4, 7], [0, 3, 6]]
+const playAgain = $('.play-again')
+const resetGame = $('.game-reset')
+const cells = document.querySelectorAll('.game-cell')
 const players = [playerOne, playerTwo]
-const boardGame = Array.from(Array(9).keys());
+const winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [6, 4, 2], [2, 5, 8], [1, 4, 7], [0, 3, 6]]
 
-players.forEach((player) => player.addEventListener('click', () => startGame(player)))
-playAgain.forEach((player) => player.addEventListener('click', () => startGame(player)))
+let user
+let computer
+let boardGame
+
+players.forEach((player) => player.addEventListener('click', () => selectPlayer(player)))
+resetGame.addEventListener('click', () => {
+  // if (computer !== '✖') {
+  //   for (const cell of cells) {
+  //     cell.innerHTML = ''
+  //   }
+  //   turn(minMax(boardGame, computer).index, computer)
+  //   // console.log(minMax(boardGame, computer).index);
+  //   // console.log(turn(minMax(boardGame, computer).index, computer))
+  //   // console.log(user, 'user');
+  //   // console.log(computer, 'computer');
+  // }
+})
+playAgain.addEventListener('click', () => {
+  addClasslistAndSetTime('fadeOut', winner, 'none', 0)
+  addClasslistAndSetTime('fadeIn', gameSelection, 'block', 300)
+
+  for (const cell of cells) {
+    cell.innerHTML = ''
+  }
+})
+
+const addClasslistAndSetTime = (effect, element, display, time) => {
+  element.classList.add(effect)
+  return setTimeout(() => { element.style.display = display }, time)
+}
 
 const selectPlayer = (player) => {
+  const span = `<span class='dot big-dot'></span>`
+  const { textContent } = player
+  user = (textContent === '✖') ? '✖' : span
+  computer = (user === '✖') ? span : '✖'
+  boardGame = Array.from(Array(9).keys())
+
   for (const cell of cells) {
+    cell.classList.add('center')
     cell.addEventListener('click', handleClick, false)
   }
-  // winner.classList.remove('fadeIn');
-  // winner.classList.add('fadeOut');
-  // setTimeout(() => { winner.style.display = 'none' }, 290);
 
-  gameSelection.style.display = 'none';
-  gameSelection.classList.add('fadeIn');
-  setTimeout(() => { gameSelection.style.display = 'none' }, 500);
+  if (computer === '✖') turn(minMax(boardGame, computer).index, computer)
+  startGame()
+  console.log('aqui no lleha');
+  addClasslistAndSetTime('fadeOut', gameSelection, 'none', 0)
+  addClasslistAndSetTime('fadeIn', gameBoard, 'block', 300)
+}
 
-  gameBoard.style.display = 'block';
-  gameBoard.classList.remove('fadeIn');
-  gameBoard.classList.add('fadeOut');
-  setTimeout(() => { gameBoard.style.display = 'block' }, 290);
+const startGame = () => {
+  addClasslistAndSetTime('fadeOut', winner, 'none', 0)
+  addClasslistAndSetTime('fadeOut', gameBoard, 'block', 300)
+  addClasslistAndSetTime('fadeIn', gameSelection, 'none', 0)
 
-
-  // console.log(player)
-  // if (player === '✖') console.log(player)
-  // if (player === '✖') turn(spot(), player)
-  // cells.forEach((cell) => cell.addEventListener('click', handleClick, false))
-
+  for (const cell of cells) {
+    cell.style.color = '#000'
+    cell.style.background = 'linear-gradient(130deg, #005f9eb3 0%, #00111cb3 30%, #00111cb3 70%, #005f9eb3 100%)';
+  }
 }
 
 const turn = (spaceId, player) => {
-  console.log(player, 'desde turn')
-  boardGame[spaceId] = player;
-  // $(`#${spaceId}`).innerHTML = player;
-  document.getElementById(spaceId).innerHTML = player;
+  boardGame[spaceId] = player
+  $$(spaceId).innerHTML = player
   const gameWon = hasWinner(boardGame, player)
   if (gameWon) gameOver(gameWon)
-  isTie()
+  else isTie()
 }
 
-const startGame = (player) => {
-  // boardGame = Array.from(Array(9).keys());
-  winner.classList.remove('fadeIn');
-  winner.classList.add('fadeOut');
-  setTimeout(() => { winner.style.display = 'none' }, 0);
-
-  const { textContent } = player;
-  console.log(minMax(boardGame, computer), 'start game')
-  if (textContent === '✖') {
-    user = '✖'
-    computer = 'O'
-    selectPlayer(user)
-    turn(minMax(boardGame, computer).index, user)
-  }
-  user = 'O'
-  computer = '✖'
-  selectPlayer(user)
-
-
-
-  // gameSelection.style.display = 'none';
-  // gameSelection.classList.add('fadeIn');
-  // setTimeout(() => { gameSelection.style.display = 'none' }, 290);
-
-  // gameBoard.style.display = 'block';
-  // gameBoard.classList.remove('fadeIn');
-  // gameBoard.classList.add('fadeOut');
-  // setTimeout(() => { gameBoard.style.display = 'block' }, 290);
-
-  // for (const cell of cells) {
-  //   cell.innerHTML = '';
-  //   cell.style.color = '#000';
-  //   cell.style.background = '#ff9eb150';
-  // }
-}
-
-
-const handleClick = (gameSpace) => {
-  const { id } = gameSpace.target
-  // console.log(minMax(gameBoard, computer).index)
+const handleClick = (event) => {
+  const { id } = event.target
   if (typeof boardGame[id] === 'number') {
     turn(id, user)
     if (!hasWinner(boardGame, user) && !isTie()) {
       setTimeout(() => {
         turn(minMax(boardGame, computer).index, computer)
-      }, 500);
+      }, 500)
     }
   }
 }
 
 const gameOver = gameWon => {
-  for (let index of winCombos[gameWon.index]) {
-    document.getElementById(index).style.color = '#FFF';
-    document.getElementById(index).style.backgroundColor = '#B33951';
+  for (let idx of winCombos[gameWon.index]) {
+    $$(idx).style.color = '#fff'
+    $$(idx).style.backgroundColor = '#B33951'
   }
 
   for (let cell of cells) {
-    cell.removeEventListener('click', handleClick, false);
+    cell.removeEventListener('click', handleClick, false)
   }
-
-  declareWinner(gameWon.player === user ? "You Won The Game!" : "Computer Won The Game!");
+  const gameWinner = gameWon.player === user
+    ? "You Won The Game!"
+    : "Computer Won The Game!"
+  declareWinner(gameWinner)
 }
 
 const declareWinner = message => {
-  winner.querySelector('h4').innerHTML = message;
+  winner.querySelector('h4').innerHTML = message
 
   setTimeout(() => {
-
-    gameBoard.classList.remove('fadeIn');
-    gameBoard.classList.add('fadeOut');
-    setTimeout(() => { gameBoard.style.display = 'none' }, 290);
-
-    winner.classList.add('fadeIn');
-    setTimeout(() => { winner.style.display = 'block' }, 290);
-
-  }, 1500);
+    addClasslistAndSetTime('fadeOut', gameBoard, 'none', 0)
+    addClasslistAndSetTime('fadeIn', winner, 'block', 300)
+  }, 1500)
 }
 
 const isTie = () => {
   if (emptySquares().length === 0) {
-    gameSelection.classList.remove('fadeOut');
-
+    gameSelection.classList.remove('fadeOut')
     for (let cell of cells) {
-      cell.style.backgroundColor = "#B33951";
-      cell.removeEventListener('click', handleClick, false);
+      cell.removeEventListener('click', handleClick, false)
     }
-
-    declareWinner("The Game Is Tie!");
-    return true;
+    declareWinner("The Game Is Tie!")
+    return true
   }
-
-  return false;
+  return false
 }
 
-
 const hasWinner = (board, player) => {
-  const spaces = board.reduce((acc, ele, idx) => (ele === player) ? acc.concat(idx) : acc, []);
-  let gameWon = null;
+  const spaces = board.reduce((acc, ele, idx) => (ele === player) ? acc.concat(idx) : acc, [])
+  let gameWon = null
 
   for (let [index, winComboSpaces] of winCombos.entries()) {
     if (winComboSpaces.every(elem => spaces.indexOf(elem) > -1)) {
-      gameWon = { index: index, player: player };
-      break;
+      gameWon = { index, player }
+      break
     }
   }
-
-  return gameWon;
+  return gameWon
 }
-const emptySquares = () => boardGame.filter((elm, i) => i === elm);
+const emptySquares = () => boardGame.filter((elm, i) => i === elm)
 
 const minMax = (testBoard, player) => {
-  // console.log('aqui entra')
-  const openSpaces = emptySquares();
-  const moves = [];
-  let bestMove;
-  let bestScore;
+  const openSpaces = emptySquares()
+  const moves = []
+  let bestMove
+  let bestScore
 
-  if (hasWinner(testBoard, user)) return { score: -10 };
-  else if (hasWinner(testBoard, computer)) return { score: 10 };
-  else if (openSpaces.length === 0) return { score: 0 };
+  if (hasWinner(testBoard, user)) return { score: -10 }
+  else if (hasWinner(testBoard, computer)) return { score: 10 }
+  else if (openSpaces.length === 0) return { score: 0 }
 
   for (let i = 0; i < openSpaces.length; i++) {
-    const move = {};
-    move.index = testBoard[openSpaces[i]];
-    testBoard[openSpaces[i]] = player;
-    console.log(minMax(testBoard, user))
+    const move = {}
+    move.index = testBoard[openSpaces[i]]
+    testBoard[openSpaces[i]] = player
 
-    if (player === computer) move.score = minMax(testBoard, user)?.score;
-    else move.score = minMax(testBoard, computer)?.score;
+    if (player === computer) move.score = minMax(testBoard, user)?.score
+    else move.score = minMax(testBoard, computer)?.score
 
-    testBoard[openSpaces[i]] = move.index;
+    testBoard[openSpaces[i]] = move.index
 
     if ((player === computer && move.score === 10) || (player === user && move.score === -10))
-      return move;
-    else moves.push(move)
+      return move
+    moves.push(move)
   }
 
   const checkScore = (array, bestScoreValue) => {
     bestScore = bestScoreValue
     for (let i = 0; i < array.length; i++) {
       if (array[i].score > bestScore) {
-        bestScore = array[i].score;
-        bestMove = i;
+        bestScore = array[i].score
+        bestMove = i
       }
     }
   }
-  if (player === computer) checkScore(moves, -1000);
-  else checkScore(moves, 1000);
-  return moves[bestMove];
-
-  // if (player === computer) {
-  //   // bestScore = -1000;
-  //   // for (let i = 0; i < moves.length; i++) {
-  //   //   if (moves[i].score > bestScore) {
-  //   //     bestScore = moves[i].score;
-  //   //     bestMove = i;
-  //   //   }
-  //   // }
-  //   ciclofor(moves, -1000)
-
-  // } else {
-  //   ciclofor(moves, 1000)
-  //   // bestScore = 1000;
-  //   // for (let i = 0; i < moves.length; i++) {
-  //   //   if (moves[i].score < bestScore) {
-  //   //     bestScore = moves[i].score;
-  //   //     bestMove = i;
-  //   //   }
-  //   // }
-
-  // }
+  if (player === computer) checkScore(moves, -1000)
+  checkScore(moves, 1000)
+  return moves[bestMove]
 }
-
